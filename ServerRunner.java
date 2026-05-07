3aaa1dcb1df02db39daa545cf152df3f6b2fce48
yasen5 <167659798+yasen5@users.noreply.github.com>
2026-05-06 20:13:16 -0700

    Fix gradle and run correct tasks

--- ServerRunner.java
+++ ServerRunner.java
@@ -2,8 +2,8 @@ import src.net.GameServer;
 
 public class ServerRunner {
     public static void main(String[] args) throws Exception {
-        int port = args.length > 0 ? Integer.parseInt(args[0]) : 5050;
-        int rebelPlayers = args.length > 1 ? Integer.parseInt(args[1]) : 2;
+        int port = 5050;
+        int rebelPlayers = 1;
         new GameServer(port, rebelPlayers).run();
     }
 }
